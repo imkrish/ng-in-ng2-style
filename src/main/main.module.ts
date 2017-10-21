9ac074d9f97a0b32b089f068dd1960c6658a7bb2
imkrish <imkrish.developer@gmail.com>
2017-10-21 17:51:11 +1300

    restructure again

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -2,9 +2,11 @@ import * as angular from 'angular'
 import { MainComponent } from './main.component'
 import { HomeModule } from './home/home.module'
 import { MainHeaderComponent } from './main.header.component'
+import { RegisterModalComponent } from './register/register-modal.component'
 
 export const MainModule = angular.module('main', [
         HomeModule.name
     ])
     .component('main', MainComponent)
     .component('mainHeader', MainHeaderComponent)
+    .component('registerModal', RegisterModalComponent)
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import * as angular from 'angular'
 import { ModalService } from './modals/modal.service'
-import { RegisterModalComponent } from './modals/register-modal.component'
 
 export const SharedModule = angular.module('shared', [])
-    .component('registerModal', RegisterModalComponent)
     .service('modalService', ModalService)
